8da4a77cdfd7498726f13541041100041ccd013a
lyfgta <59587971+lyfgta@users.noreply.github.com>
2022-05-01 15:55:38 +0800

    Update indexseach.js

--- js/indexseach.js
+++ js/indexseach.js
@@ -1,4 +1,6 @@
+
 //显示隐藏div
+
         function $(id) {
             return document.getElementById(id)
         }
@@ -9,15 +11,24 @@
         }
         
         // 方法一：使用dom0方式
+
         // oMenu.onclick = function(e){
+
         //     stopFunc(e);
+
         //     oModel.style.display = "block";
-        // }
-        // oModel.onclick = function(e){
-        //     stopFunc(e);
+
         // }
 
+        // oModel.onclick = function(e){
+
+        //     stopFunc(e);
+
+        // }
+
+
         //方法二：使用dom2方式
+
         oMenu.addEventListener('click', function (e) {
             stopFunc(e);
             oModel.style.display = "block";
@@ -27,12 +38,45 @@
         }, false)
         
         //阻止事件向上传递
+
         function stopFunc(e) { 
             e.stopPropagation ? e.stopPropagation() : e.cancelBubble = true;
         }
 
 //本地搜索
+
 function myFunction() {
     // 声明变量
+
     var input, filter, ul, li, a, i;
-    input 
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // 循环所有列表，查找匹配项
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+//自动提交给百度
+
+        (function(){
+            var bp = document.createElement('script');
+            var curProtocol = window.location.protocol.split(':')[0];
+            if (curProtocol === 'https') {
+                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+            }
+            else {
+                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+            }
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(bp, s);
+        })();
